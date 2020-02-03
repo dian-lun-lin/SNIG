af6ffa9aa9675e08dbcf1949751b50692e030f28
Luan <cyc4542000@gmail.com>
2020-02-03 16:26:20 -0700

    chage bias to single value, not vector

--- Baseline/C++/inferenceReLUvec.cpp
+++ Baseline/C++/inferenceReLUvec.cpp
@@ -3,13 +3,13 @@
 #include<Eigen/Sparse>
 
 const float YMAX = 32;
-Eigen::SparseMatrix<float> inferenceReLUvec(const std::vector<Eigen::SparseMatrix<float> >& W, const std::vector<float>& bias, const Eigen::SparseMatrix<float>& Y0){
+Eigen::SparseMatrix<float> inferenceReLUvec(const std::vector<Eigen::SparseMatrix<float> >& W, const float &bias, const Eigen::SparseMatrix<float>& Y0){
 	Eigen::SparseMatrix<float> Y(Y0);
 	Eigen::SparseMatrix<float> Z(Y.rows(), Y.cols());
 	Z.reserve(Y.nonZeros());
 	for(size_t i = 0; i < W.size(); ++i){
 		Z = (Y * W[i]).pruned();
-		Z.coeffs() += bias[i];
+		Z.coeffs() += bias;
 		Y = Z.unaryExpr([] (float a) {
 				if(a < 0) return 0.0f;
 				else if(a > YMAX) return YMAX;
