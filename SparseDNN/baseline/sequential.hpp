fb5d1b06f6886c08e18d7891e1d6a7d9ff732e72
Luan <cyc4542000@gmail.com>
2020-02-06 21:50:12 -0700

    add sequential class to run within serial CPU

--- SparseDNN/baseline/sequential.hpp
+++ SparseDNN/baseline/sequential.hpp
@@ -1,14 +1,13 @@
 #pragma once
 
 #include <iostream>
-#include <experimental/filesystem>
+#include <SparseDNN/util/file.hpp>
+#include <Eigen/Sparse>
 
 namespace std {
-  namespace filesystem = experimental::filesystem;
+	namespace fs = experimental::filesystem;
 }
-
 namespace sparse_dnn {
-
 template <typename T>
 class Sequential {
 
@@ -19,22 +18,22 @@ class Sequential {
   );
 
   private:
-
+	 const std::fs::path& _weight_path;
+	 const size_t _num_inputs;
     const size_t _num_neurons;
     const size_t _num_layers;
     const T _bias;
 
-    // TODO:
-    // _read_
-
-    std::string _read_file_to_string();
+	 Eigen::SparseMatrix<T> read_weight_or_input const  (const std::fs::path& path, bool is_input);
+	 Eigen::SpaseVector<T> read_golden const (const std::fs::path& path);
 
   public:
 
     Sequential(
-      const std::filesystem::path& path,
-      size_t num_neurons,
-      size_t num_layers,
+      const std::fs::path& weight_path,
+		const size_t num_inputs,
+      const size_t num_neurons,
+      const size_t num_layers,
       T bias
     );
 
@@ -44,11 +43,8 @@ class Sequential {
     size_t num_layers() const;
     T bias() const;
 
-    float infer(
-      const std::filesystem::path& input, 
-      const std::filesystem::path& golden
-    ) const;
-
+    void infer() const;
+	 void check_passed(const Eigen::SparseMatrix& output_mat, const std::fs::path& golden) const;
 };
 
 // ----------------------------------------------------------------------------
@@ -57,24 +53,108 @@ class Sequential {
 
 template <typename T>
 Sequential<T>::Sequential(
-  const std::filesystem::path& path,
-  size_t num_neurons,
-  size_t num_layers,
+  const std::fs::path& weight_path,
+  const size_t num_inputs,
+  const size_t num_neurons,
+  const size_t num_layers,
   T bias
 ) :
+  _weight_path {weight_path},
+  _num_inputs {num_inputs},
   _num_neurons {num_neurons},
   _num_layers {num_layers},
   _bias {bias}
 {
-  // TODO
-  std::cout << "constructing a sequential baseline\n";
+  std::cout << "Constructing a sequential baseline.\n";
 }
 
 template <typename T>
 Sequential<T>::~Sequential() {
-  // TODO
 }
 
+template <typename T>
+inline Sequential<T>:: Eigen::SparseMatrix<T> read_weight_or_input const  (const std::fs::path& path, bool is_input) {
+		std::string line;
+		std::vector<T> tripletList;
+		Eigen::SParseMatrix<T> mat;
+		if(is_input){
+			mat.conservativeResize(_num_inputs, _num_neurons);
+			tripletList.reserve((_num_inputs*_num_neurons)/200);
+		}
+		else{
+			mat.conservativeResize(_num_neurons, _num_neurons);
+			tripletList.reserve((_num_neurons^2)/200);
+		}
+		std::istringstream read_s(read_file_to_string(path));
+		while(std::getline(read_s, line)){
+			std::istringstream lineStream(line);
+			std::string token;
+			std::vector<T> tokens;
+			while(std::getline(lineStream, token, '\t'))	tokens.push_back(std::stof(token));
+			tripletList.push_back(T(tokens[0] - 1, tokens[1] - 1, tokens[2]));
+		}
+		mat.reserve(tripletList.size());
+		mat.setFromTriplets(tripletList.begin(), tripletList.end());
+		mat.makeCompressed();
+		return mat;
+}
+
+template <typename T>
+inline Sequential<T>:: Eigen::SpaseVector<T> read_golden(const std::fs::path& path) const {
+	std::string line;
+	std::istringstream read_s(read_file_to_string(path));
+	Eigen::SparseVector<T> v(_num_inputs);
+	v.reserve(_num_inputs / 200);
+	while(std::getline(f, line)) v.insert(std::stoi(line) - 1) = 1;
+	return v;
+}
+
+
+template <typename T>
+inline Sequential::void infer(
+      const std::fs::path& input, 
+      const std::fs::path& golden)
+     const {
+   std::cout << "Loading the weight and input..............\n";
+	Eigen::SparseMatrix<T> input_mat(_num_inputs, _num_neurons);
+	Eigen::SparseMatrix<T> weight_mat(_num_neurons, _num_neurons);
+	read_weight_and_input(input, input_mat, weight_mat);
+	Eigen::SparseMatrix<T> Y(input_mat);
+	Eigen::SparseMatrix<T> Z(Y.rows(), Y.cols());
+	Z.reserve(Y.nonZeros());
+	std::cout << "Start inference............................\n";
+	for(size_t i = 0; i < W.size(); ++i){
+		Z = (Y * W[i]).pruned();
+		Z.coeffs() += _bias;
+		Y = Z.unaryExpr([] (T a) {
+				if(a < 0) return 0.0f;
+				else if(a > YMAX) return YMAX;
+				return a;
+				});
+	}
+	Y.makeCompressed();
+	std::cout << "Done.\n";
+	check_passed(Y, golden);
+}
+template <typename T>
+inline Sequential::void check_passed(const Eigen::SparseMatrix<T>& output_mat, const std::fs::path& golden) const {
+	std::cout << "Checking correctness........................\n";
+	Eigen::SpaseVector<T> golden_mat(_num_inputs);
+	golden_mat = read_golden(golden);
+	Eigen::SparseVector<T> output_mat = Eigen::MatrixXf(scores).rowwise().sum().sparseView();
+	output_mat = output_mat.unaryExpr([] (T a) {
+			if(a > 0) return 1;
+			else return 0;
+			});
+	Eigen::SparseVector<T> diff_mat = gloden_mat - output_mat;
+	diff_mat = diff_mat.pruned();
+	if(diff_mat.nonZeros())
+		std::cout << "Challenge FAILED\n";
+	else
+		std::cout << "Challenge PASSED\n" ;
+}
+
+}
 }  // end of namespace sparse_dnn ---------------------------------------------
 
 
