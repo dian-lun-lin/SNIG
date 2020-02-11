62c8220afc1141ae35fa3baaeb00ac5cddf71670
cyc4542015 <cyc4542000@gmail.com>
2020-02-11 13:02:03 -0700

    add file read/write

--- SparseDNN/utility/reader.hpp
+++ SparseDNN/utility/reader.hpp
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <iostream>
-#include <SparseDNN/utility/file.hpp>
+#include <experimental/filesystem>
+#include <fstream>
 #include <Eigen/Sparse>
 #include <vector>
+#include <string>
 
 namespace std {
 	namespace fs = experimental::filesystem;
@@ -28,7 +30,12 @@ class Reader {
       const std::string& s,
       const size_t rows,
       const size_t cols
-      ) const;
+    ) const;
+    std::string _read_file_to_string(const std::fs::path& path) const;
+    void _write_file_from_string(
+        const std::fs::path& path,
+        const std::string& s
+    ) const;
 	
 	public:
 
@@ -36,9 +43,13 @@ class Reader {
       const std::fs::path& weight_path,
       const std::fs::path& input_path,
       const std::fs::path& golden_path
-      );
+    );
 
-    std::vector<Eigen::SparseMatrix<T> > read_weight_and_input(size_t num_inputs, size_t num_neurons, size_t num_layers=1024) const;
+    std::vector<Eigen::SparseMatrix<T> > read_weight_and_input(
+        size_t num_inputs,
+        size_t num_neurons,
+        size_t num_layers=1024
+    ) const;
     Eigen::SparseMatrix<T> read_golden(size_t num_inputs) const;
 
 };
@@ -95,7 +106,7 @@ Eigen::SparseMatrix<T> Reader<T>::
 read_golden(size_t num_inputs) const {
 
   std::string line;
-  std::istringstream read_s(read_file_to_string(_golden_path));
+  std::istringstream read_s(_read_file_to_string(_golden_path));
   Eigen::SparseMatrix<T> mat(num_inputs, 1);
   mat.reserve(num_inputs / 200);
   while(std::getline(read_s, line)) mat.insert(std::stoi(line) - 1, 1) = 1;
@@ -104,14 +115,18 @@ read_golden(size_t num_inputs) const {
 
 template <typename T>
 std::vector<Eigen::SparseMatrix<T> > Reader<T>::
-read_weight_and_input(size_t num_inputs, size_t num_neurons, size_t num_layers) const {
+read_weight_and_input(
+    size_t num_inputs, 
+    size_t num_neurons,
+    size_t num_layers
+) const {
 
   std::vector<Eigen::SparseMatrix<T> > mats;
   mats.reserve(num_layers + 1);
   std::cout << "Loading the weight and input..............";
 
   //read input
-  std::string input_str = read_file_to_string(_input_path);
+  std::string input_str = _read_file_to_string(_input_path);
   mats.push_back(_tsv_string_to_matrix(input_str, num_inputs, num_neurons));
 
   //read weights
@@ -119,11 +134,42 @@ read_weight_and_input(size_t num_inputs, size_t num_neurons, size_t num_layers) 
     std::fs::path p = _weight_path;
     p /= "n" + std::to_string(num_neurons) + "-l"
       + std::to_string(i + 1) + ".tsv";
-    std::string data_str = read_file_to_string(p);
+    std::string data_str = _read_file_to_string(p);
     mats.push_back(_tsv_string_to_matrix(data_str, num_neurons, num_neurons));
   }
   std::cout << "Done\n";
   return mats;
+}
+
+template<typename T>
+std::string Reader<T>:: _read_file_to_string(const std::fs::path& path) const {
+  
+  using namespace std::literals::string_literals;
+
+	std::ifstream f{ path };
+  if(!f){
+    throw std::runtime_error("cannot open the file"s + path.c_str());
+  }
+
+	const auto fsize = std::fs::file_size(path);
+  std::string result;
+	result.reserve(fsize);
+	f.read(&result[0], fsize);
+	return result;
+}
+
+template<typename T>
+void Reader<T>::_write_file_from_string(
+    const std::fs::path& path,
+    const std::string& s
+) const {
+  using namespace std::literals::string_literals;
+
+	std::ofstream f{ path };
+  if(!f){
+    throw std::runtime_error("cannot open the file"s + path.c_str());
+  }
+	f.write(&s[0], std::fs::file_size(path));
 }
 
 } // end of namespace sparse_dnn-----------------------------------------------
