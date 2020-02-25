0e0fdbb2785903c97980b5cc86a110f49924fe19
cyc4542015 <cyc4542000@gmail.com>
2020-02-24 21:42:17 -0700

    add GPU

--- main/main.cu
+++ main/main.cu
@@ -21,7 +21,7 @@ int main(int argc, char* argv[]) {
   std::string mode = "sequential";
   app.add_option("-m, --mode", 
     mode, 
-    "select mode(sequential/CPU_parallel), default is sequential");
+    "select mode(sequential/), default is sequential");
 
   std::fs::path weight_path;
   app.add_option("-w, --weight", weight_path, "weight directory path")
@@ -44,45 +44,18 @@ int main(int argc, char* argv[]) {
   float bias = -0.3f;
   app.add_option("-b, --bias", bias, "bias");
 
-  //for testing
-  std::fs::path input_path("/home/dian-lun/dian/GraphChallenge_SparseDNN/dataset/MNIST/sparse-images-1024.tsv");
-  app.add_option(
-      "--input",
-      input_path, 
-      "input tsv file path, default is 1024"
-  );
-
-  std::fs::path golden_path("/home/dian-lun/dian/GraphChallenge_SparseDNN/dataset/MNIST/neuron1024-l120-categories.tsv");
-  app.add_option(
-      "--golden",
-      golden_path, 
-      "golden tsv file path, default is 1024/120"
-  );
-
   CLI11_PARSE(app, argc, argv);
 
+  std::fs::path input_path("/home/dian-lun/dian/GraphChallenge_SparseDNN/dataset/MNIST/sparse-images-1024.tsv");
+  std::fs::path golden_path("/home/dian-lun/dian/GraphChallenge_SparseDNN/dataset/MNIST/neuron1024-l120-categories.tsv");
   //Data parallel mode
-  Eigen::SparseVector<float> result;
-  if(mode == "CPU_parallel"){
-    sparse_dnn::CPUParallel<float> cpu_parallel(
-        weight_path,
-        bias,
-        num_neurons_per_layer,
-        num_layers
-        );
-    result = cpu_parallel.infer(input_path, 60000);
-  }
-  else if(mode == "sequential"){
-    //Sequential mode
-    sparse_dnn::Sequential<float> sequential(
-      weight_path, 
+  sparse_dnn::GPUParallel<float> GPU_parallel(
+      weight_path,
       bias,
-      num_neurons_per_layer, 
+      num_neurons_per_layer,
       num_layers
-    );
-    result = sequential.infer(input_path, 60000);
-  }
-
+      );
+  auto result = GPU_parallel.infer(input_path, 60000);
   auto golden = sparse_dnn::read_golden<float>(golden_path, 60000);
   if(sparse_dnn::is_passed<float>(result, golden)){
     std::cout << "CHALLENGE PASSED\n";
