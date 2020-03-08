17a85d56d0e38d2b1c03230c4425983fe4e39ff1
cyc4542015 <cyc4542000@gmail.com>
2020-03-08 16:39:45 -0600

    check cuda error

---
+++ SparseDNN/utility/cuda_error.hpp
@@ -0,0 +1,16 @@
+#include <assert.h>
+
+// Convenience function for checking CUDA runtime API results
+// can be wrapped around any runtime API call. No-op in release builds.
+inline
+cudaError_t checkCuda(cudaError_t result)
+{
+#if defined(DEBUG) || defined(_DEBUG)
+  if (result != cudaSuccess) {
+    fprintf(stderr, "CUDA Runtime Error: %s\n", 
+            cudaGetErrorString(result));
+    assert(result == cudaSuccess);
+  }
+#endif
+  return result;
+}
