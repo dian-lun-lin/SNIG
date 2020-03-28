8beacb548541b6fb5562ee586ca40372eaada960
cyc4542015 <cyc4542000@gmail.com>
2020-03-28 14:41:05 -0600

    add utilities

---
+++ SparseDNN/utility/utility.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include <functional>
+#include <algorithm>
+
+namespace sparse_dnn {
+
+inline
+float average_zero_percent_in_non_empty_rows(
+  int* rlenY,
+  int* rowsY,
+  size_t num_features,
+  size_t nerowsY
+);
+
+//-----------------------------------------------------------------------------
+//Definition of utility function
+//-----------------------------------------------------------------------------
+
+inline
+float average_zero_percent_in_non_empty_rows(
+  int* rlenY,
+  int* rowsY,
+  size_t num_features,
+  size_t nerowsY
+) {
+  int total_zero{0};
+  for(int i = 0; i < nerowsY; ++i) {
+    total_zero += num_features - rlenY[rowsY[i]];
+  }
+  return (100 * (total_zero / float(num_features * nerowsY)));
+
+};
+
+}// end of namespace sparse_dnn ----------------------------------------------
