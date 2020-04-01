e365abe6c096bbe19a5d8e40fd3e1fca717f9801
cyc4542015 <cyc4542000@gmail.com>
2020-04-01 17:08:04 -0600

    add cpp-taskflow

---
+++ 3rd-party/taskflow/declarations.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+namespace tf {
+
+// ----------------------------------------------------------------------------
+// forward declarations
+// ----------------------------------------------------------------------------
+
+// taskflow
+class Node;
+class Graph;
+class FlowBuilder;
+class Subflow;
+class Task;
+class TaskView;
+class Taskflow;
+class Topology;
+class Executor;
+
+// cudaflow
+class cudaNode;
+class cudaGraph;
+class cudaTask;
+class cudaFlow;
+
+
+
+}  // end of namespace tf -----------------------------------------------------
+
+
+
+
