6e1254afa7913caa4f9764bc642798d384fdf55b
cyc4542015 <cyc4542000@gmail.com>
2020-04-05 00:25:25 -0600

    remove bug

--- SparseDNN/parallel/task.hpp
+++ SparseDNN/parallel/task.hpp
@@ -314,6 +314,85 @@ void resize_CPU(CSRMatrix<T>& target, int rows) {
   std::remove(target.col_array, target.col_array + nnz, -1);
 }
 
+//template <typename T>
+//__global__ 
+//void baseline_inference(
+  //const T* Y0,
+  //const size_t nerowsY,
+  //const int* rowsY0,
+  //int* rlenY0,
+  //const size_t COL_BLK,
+  //const size_t N_SLAB,
+  //const size_t num_neurons_per_layer,
+  //const int* roffW,
+  //const int* colsW,
+  //const T* valsW,
+  //const T bias,
+  //T* Y1,
+  //int* rlenY1
+//) {
+
+  //if(blockIdx.x >= nerowsY) {
+    //return;
+  //}
+
+  //extern  __shared__ T shRow[];
+
+  //int tid = threadIdx.y * blockDim.x + threadIdx.x;
+  //int rid = rowsY0[blockIdx.x];
+  //__syncthreads();
+  //if(tid == 0) {
+    //rlenY0[rid] = 0;
+    //rlenY1[rid] = 0;
+  //}
+  //size_t nn_per_thread = num_neurons_per_layer / (blockDim.x * blockDim.y);
+  //size_t row_loc;
+  //int cur_loc;
+  //T valY;
+
+  //T local_Y[64];
+  //for(size_t k = 0; k < nn_per_thread; ++k) {
+    //local_Y[k] = Y0[rid * num_neurons_per_layer + tid + k * blockDim.x * blockDim.y];
+  //}
+  //for(size_t i = 0; i < N_SLAB; i++) {
+    //__syncthreads();
+    //for(size_t j = threadIdx.x; j < COL_BLK; j++) {
+      //shRow[j] = 0;  
+    //}
+    //__syncthreads();
+    //for(size_t cur_v = 0; cur_v < nn_per_thread; ++cur_v) {
+      //for(int t = 0; t < blockDim.x; ++t) {
+        //valY = __shfl_sync(0xffffffff, local_Y[cur_v], t, blockDim.x);
+        //cur_loc = __shfl_sync(0xffffffff, tid, t, blockDim.x);
+        //__syncwarp();
+        //if(valY == 0) {
+          //continue;
+        //}
+        //row_loc = cur_loc + cur_v * blockDim.x * blockDim.y;
+        //int begOffW = roffW[i * num_neurons_per_layer + row_loc] + threadIdx.x;
+        //int endOffW = roffW[i * num_neurons_per_layer + row_loc + 1];
+        //for(int k = begOffW; k < endOffW; k += blockDim.x) {
+          //int colW = colsW[k];
+          //T valW = valsW[k];
+          //atomicAdd(&shRow[colW - i * COL_BLK], valY * valW);
+        //}
+      //}
+    //}
+    //__syncthreads();
+    //int count = 0;
+    //for(size_t j = 0; j < COL_BLK; j += blockDim.x * blockDim.y) {
+      //T v = j + tid < COL_BLK ? shRow[j + tid] + bias : -1;
+      //count += __syncthreads_count(v > 0);
+      //if(j + tid < COL_BLK) {
+        //Y1[rid * num_neurons_per_layer + i * COL_BLK + j + tid] = min(T(32), max(T(0), v));
+      //}
+    //}
+    //if(tid == 0) {
+      //rlenY1[rid] += count;
+    //}
+  //}
+//}
+
 template <typename T>
 __global__ 
 void baseline_inference(
@@ -345,36 +424,24 @@ void baseline_inference(
     rlenY0[rid] = 0;
     rlenY1[rid] = 0;
   }
-  size_t nn_per_thread = num_neurons_per_layer / (blockDim.x * blockDim.y);
-  size_t cur_loc;
-  size_t row_loc;
-  T valY;
 
-  T local_Y[16];
-  for(size_t k = 0; k < nn_per_thread; ++k) {
-    local_Y[k] = Y0[rid * num_neurons_per_layer + tid + k * blockDim.x * blockDim.y];
-  }
   for(size_t i = 0; i < N_SLAB; i++) {
     __syncthreads();
     for(size_t j = threadIdx.x; j < COL_BLK; j++) {
       shRow[j] = 0;  
     }
     __syncthreads();
-    for(size_t cur_v = 0; cur_v < nn_per_thread; ++cur_v) {
-      for(size_t t = 0; t < blockDim.x; ++t) {
-        valY = __shfl_sync(0xffffffff, local_Y[cur_v], t, blockDim.x);
-        if(valY == 0) {
-          continue;
-        }
-        cur_loc = __shfl_sync(0xffffffff, tid, t, blockDim.x);
-        row_loc = cur_loc + cur_v * blockDim.x * blockDim.y;
-        int begOffW = roffW[i * num_neurons_per_layer + row_loc] + threadIdx.x;
-        int endOffW = roffW[i * num_neurons_per_layer + row_loc + 1];
-        for(int k = begOffW; k < endOffW; k += blockDim.x) {
-          int colW = colsW[k];
-          T valW = valsW[k];
-          atomicAdd(&shRow[colW - i * COL_BLK], valY * valW);
-        }
+    for(size_t j = threadIdx.y; j < num_neurons_per_layer; j += blockDim.y) {
+      T valY = Y0[rid * num_neurons_per_layer + j];
+      if(valY == 0) {
+        continue;
+      }
+      int begOffW = roffW[i * num_neurons_per_layer + j] + threadIdx.x;
+      int endOffW = roffW[i * num_neurons_per_layer + j + 1];
+      for(int k = begOffW; k < endOffW; k += blockDim.x) {
+        int colW = colsW[k];
+        T valW = valsW[k];
+        atomicAdd(&shRow[colW - i * COL_BLK], valY * valW);
       }
     }
     __syncthreads();
