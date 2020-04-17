7e394115b1dd0184125d8a730cfd72aa5261ee89
Dian-Lun Lin <cyc4542000@gmail.com>
2020-04-17 00:46:58 -0600

    add is_test option

--- bin/executor.sh
+++ bin/executor.sh
@@ -2,6 +2,7 @@
 #       $2 neurons_per_layer
 #       $3 num_layers
 #       $4 num_device
+#       $5 is_test_data 0, 1
 
 get_bias() {
 
@@ -21,12 +22,23 @@ get_bias() {
 
 get_command() {
   get_bias $2
-  if [[ "$1" == "CPU_parallel" || "$1" == "sequential" ]]; then
-       ./main -m $1 -w ../dataset/weight/neuron$2/ --num_neurons_per_layer $2 --num_layers $3 --input ../dataset/MNIST/sparse-images-$2.b --golden ../dataset/MNIST/neuron$2-l$3-categories.b --bias $bias
-  else
-       ./main_cuda -m $1 -w ../dataset/weight/neuron$2/ --num_neurons_per_layer $2 --num_layers $3 --input ../dataset/MNIST/sparse-images-$2.b --golden ../dataset/MNIST/neuron$2-l$3-categories.b --bias $bias --num_device $4
+  if [[ $5 == 1 ]]; then
+    if [[ "$1" == "CPU_parallel" || "$1" == "sequential" ]]; then
+         ./main -m $1 -w ../dataset/weight/neuron$2/ --num_neurons_per_layer $2 --num_layers $3 --input ../dataset/MNIST/sparse-images-$2.b --golden ../dataset/MNIST/neuron$2-l$3-categories.b --bias $bias
+    else
+         ./main_cuda -m $1 -w ../dataset/test/weight/neuron$2/ --num_neurons_per_layer $2 --num_layers $3 --input ../dataset/test/MNIST/sparse-images-$2.b --golden ../dataset/test/MNIST/neuron$2-l$3-categories.b --bias $bias --num_device $4
 
+    fi
+
+  else
+    if [[ "$1" == "CPU_parallel" || "$1" == "sequential" ]]; then
+         ./main -m $1 -w ../dataset/weight/neuron$2/ --num_neurons_per_layer $2 --num_layers $3 --input ../dataset/MNIST/sparse-images-$2.b --golden ../dataset/MNIST/neuron$2-l$3-categories.b --bias $bias
+    else
+         ./main_cuda -m $1 -w ../dataset/weight/neuron$2/ --num_neurons_per_layer $2 --num_layers $3 --input ../dataset/MNIST/sparse-images-$2.b --golden ../dataset/MNIST/neuron$2-l$3-categories.b --bias $bias --num_device $4
+
+    fi
+    
   fi
 }
 
-get_command $1 $2 $3 $4
+get_command $1 $2 $3 $4 $5
